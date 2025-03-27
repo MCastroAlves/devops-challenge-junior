4f4e65007467154836a081506429c021bde2ece9
unknown <castromatheus.dev@gmail.com>
2025-03-27 15:26:32 -0300

    Alterações para teste da vaga Devops JR

--- devops_challenge.php
+++ devops_challenge.php
@@ -1,86 +1,127 @@
+<?php
 /**
- * @package Devops_challenge_Junior
- * @version 1.0
+ * Plugin para exibir letras do Tchan no rodapé do WordPress.
+ *
+ * @package     Devops_Challenge_Junior
+ * @version     1.0.0
+ * @author      Apiki WordPress
+ * @license     GPL-2.0+
  */
+
 /*
-Plugin Name: Devops challenge Júnior
+Plugin Name: Devops Challenge Júnior
 Plugin URI: https://apiki.com/
-Description: Sabe de nada, inocente! Ordinária!!
+Description: Plugin que exibe letras do Tchan no rodapé do WordPress
 Author: Apiki WordPress
-Version: 1.0
+Version: 1.0.0
+Text Domain: devops-challenge
+Domain Path: /languages
+License: GPL-2.0+
 */
 
-$global_lyrics;
-
-function apiki_segura_o_tchan() {
-	$lyrics = $global_lyrics;
-	
-	$lyrics = "Pau que nasce torto nunca se endireita
-	Menina que requebra a mãe pega na cabeça
-	Pau que nasce torto nunca se endireita
-	Menina que requebra a mãe pega na cabeça
-	Domingo ela não vai (vai, vai)
-	Domingo ela não vai não (vai, vai, vai)
-	Olha, domingo ela não vai (vai, vai)
-	Domingo ela não vai não (vai, vai, vai)
-	O pau que nasce torto nunca se endireita
-	Menina que requebra a mãe pega na cabeça
-	Pau que nasce torto nunca se endireita
-	Menina que requebra a mãe pega na cabeça
-	Segure o tchan
-	Amare o tchan
-	Segure o tchan tchan tchan tchan
-	Depois de nove meses você vê o resultado
-	Esse é o Gera Samba arrebentando no pedaço
-	Joga ela no meio, mete em cima, mete embaixo";
-
-	$lyrics = explode( "\n", $lyrics )
-
-	return wptexturize( $lyrics[ mt_rand( count( $lyrics ) - 1, 0 ) ] );
+// Previne acesso direto ao arquivo
+if (!defined('ABSPATH')) {
+    exit;
 }
 
-function devops_challenge() {
-	$chosen = $undefined_variable;
-	$lang   = '';
-	if ( 'en_' !== substr( get_user_locale(), 0, 3 ) ) {
-		$lang = ' lang="en"';
-	}
+/**
+ * Registra o domínio de texto para internacionalização
+ */
+function devops_challenge_load_textdomain() {
+    load_plugin_textdomain('devops-challenge', false, dirname(plugin_basename(__FILE__)) . '/languages');
+}
+add_action('plugins_loaded', 'devops_challenge_load_textdomain');
 
-	printf(
-		'O texto é: %s %s %s',
-		__( 'Segure o Tchan, by Apiki WordPress:' ),
-		$lang
-	);
+/**
+ * Retorna uma letra aleatória do Tchan
+ *
+ * @return string Letra aleatória do Tchan
+ */
+function devops_challenge_get_lyrics() {
+    $lyrics = array(
+        __('Pau que nasce torto nunca se endireita', 'devops-challenge'),
+        __('Menina que requebra a mãe pega na cabeça', 'devops-challenge'),
+        __('Domingo ela não vai (vai, vai)', 'devops-challenge'),
+        __('Domingo ela não vai não (vai, vai, vai)', 'devops-challenge'),
+        __('Olha, domingo ela não vai (vai, vai)', 'devops-challenge'),
+        __('Domingo ela não vai não (vai, vai, vai)', 'devops-challenge'),
+        __('Segure o tchan', 'devops-challenge'),
+        __('Amare o tchan', 'devops-challenge'),
+        __('Segure o tchan tchan tchan tchan', 'devops-challenge'),
+        __('Depois de nove meses você vê o resultado', 'devops-challenge'),
+        __('Esse é o Gera Samba arrebentando no pedaço', 'devops-challenge'),
+        __('Joga ela no meio, mete em cima, mete embaixo', 'devops-challenge')
+    );
+
+    return wptexturize($lyrics[array_rand($lyrics)]);
 }
 
-add_action( '', 'devops_challenge' );
+/**
+ * Exibe a letra do Tchan no rodapé
+ */
+function devops_challenge_display() {
+    // Verifica se estamos no admin ou no editor de blocos
+    if (is_admin() || (function_exists('is_block_editor') && is_block_editor())) {
+        return;
+    }
 
-function devop_css() {
-	echo "
-	<style type='text/css'>
-	#devop {
-		float: right;
-		padding: 5px 10px;
-		margin: 0;
-		font-size: 12px;
-		line-height: 1.6666;
-	}
-	.rtl #devop {
-		float: left;
-	}
-	.block-editor-page #devop {
-		display: none;
-	}
-	@media screen and (max-width: 782px) {
-		#devop,
-		.rtl #devop {
-			float: none;
-			padding-left: 0;
-			padding-right: 0;
-		}
-	}
-	</style>
-	";
+    // Adiciona nonce para segurança
+    $nonce = wp_create_nonce('devops_challenge_display');
+    
+    $lang = '';
+    if (strpos(get_user_locale(), 'en_') !== 0) {
+        $lang = ' lang="en"';
+    }
+
+    $chosen = devops_challenge_get_lyrics();
+    printf(
+        '<div id="devops-challenge" class="devops-challenge"%s data-nonce="%s"><span class="devops-challenge-title">%s</span> <span class="devops-challenge-lyrics">%s</span></div>',
+        esc_attr($lang),
+        esc_attr($nonce),
+        esc_html__('Segure o Tchan, by Apiki WordPress:', 'devops-challenge'),
+        esc_html($chosen)
+    );
 }
+add_action('wp_footer', 'devops_challenge_display');
 
-add_action( 'admin_head', 'devop_css' );
+/**
+ * Registra e enfileira os estilos CSS
+ */
+function devops_challenge_enqueue_styles() {
+    wp_register_style(
+        'devops-challenge-styles',
+        false,
+        array(),
+        '1.0.0'
+    );
+    
+    $css = '
+    .devops-challenge {
+        display: flex;
+        justify-content: center;
+        padding: 5px 10px;
+        margin: 0;
+        font-size: 12px;
+        line-height: 1.6666;
+    }
+    .rtl .devops-challenge {
+        float: left;
+    }
+    @media screen and (max-width: 782px) {
+        .devops-challenge,
+        .rtl .devops-challenge {
+            float: none;
+            padding-left: 0;
+            padding-right: 0;
+            text-align: center;
+        }
+    }
+    .devops-challenge-title {
+        font-weight: bold;
+    }';
+
+    wp_add_inline_style('devops-challenge-styles', $css);
+    wp_enqueue_style('devops-challenge-styles');
+}
+add_action('wp_enqueue_scripts', 'devops_challenge_enqueue_styles');
+?>
